9107d6b9ccedf2058b0c0de307a2627181341f44
SamriddhiMaurya <samriddimaurya9005@gmail.com>
2024-04-16 11:21:13 +0530

    React-Redux todo list

---
+++ src/components/Todo.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./todo.css";
+import { useSelector } from "react-redux";
+import { addTodo, deleteTodo, removeTodo } from "../actions/index";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
+const Todo = () => {
+  const [inputData, setInputData] = useState("");
+  const list = useSelector((state) => state.todoReducers.list);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <figcaption>Add your Todo here 🤘</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="✍️ Add Items..."
+              value={inputData}
+              onChange={(event) => setInputData(event.target.value)}
+            />
+            <i
+              className="fa fa-plus add-btn"
+              onClick={() => dispatch(addTodo(inputData), setInputData(""))}
+            ></i>
+          </div>
+
+          <div className="showItems">
+            {list.map((elem) => {
+              return (
+                <div className="eachItem" key={elem.id}>
+                  <h3>{elem.data}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className="far fa-trash-alt add-btn"
+                      title="Delete Item"
+                      onClick={() => {
+                        dispatch(deleteTodo(elem.id));
+                      }}
+                    ></i>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="showItem">
+            <button
+              className="btn-effect04"
+              data-sm-link="remove All"
+              onClick={() => dispatch(removeTodo())}
+            >
+              <span>Check List</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
